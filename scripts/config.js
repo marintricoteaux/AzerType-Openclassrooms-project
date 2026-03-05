c74cf7b7689ef1bf976dd45dddd803b19b106156
Marin Tricoteaux <marintcx@icloud.com>
2026-03-05 18:22:21 +0100

    JavaScript personalisé et dynamique au site

--- scripts/config.js
+++
@@ -1,4 +0,0 @@
-// Différentez listes pour jouer.
-
-const listWord = ["Bonjour", "Hublot", "Fessier", "Salut", "Coucou"];
-const listePhrases = ['Bonjour, comment allez-vous ?', 'Salut, ça va ?', 'Coucou, ça va ?']
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,68 @@
-// Le main.js s'occupe de lancer le jeu.
+// Je fais personnellement toute la logique du site en JavaScript comme entrainement
 
-// runGame();
+const listMots = ["Bonjour", "Salut", "Saucisse"];
+const listPhrases = ["Salut, ça va ?", "Je suis désolé.", "Comment vas-tu ?"]
+
+runGame();
+
+// Déclaration des fonctions
+
+function runGame() {
+    let index = 0;
+    let score = 0;
+
+    let listUsed = listMots;
+    afficherProposition(listUsed[index]);
+
+    let zoneSaisie = document.getElementById("inputEcriture");
+    let boutonValiderMot = document.getElementById("btnValiderMot");
+
+    boutonValiderMot.addEventListener("click", () => {
+        if (zoneSaisie.value === listUsed[index]) {
+            score++;
+            afficherResultat(score);
+        }
+        index++;
+        afficherProposition(listUsed[index]);
+        zoneSaisie.value = "";
+
+        if (index >= listUsed.length) {
+            afficherResultat(score, listUsed.length);
+            afficherProposition("");
+            boutonValiderMot.disabled = true;
+        }
+    });
+    
+    let radiosGameMode = document.querySelectorAll(".optionSource input");
+    for (let i = 0; i < radiosGameMode.length; i++) {
+        radiosGameMode[i].addEventListener("change", (event) => {
+            switch (event.target.value) {
+                case "1":
+                    listUsed = listMots;
+                    break;
+                case "2":
+                    listUsed = listPhrases;
+                    break;
+            }
+            index = 0;
+            score = 0;
+            afficherProposition(listUsed[index]);
+            afficherResultat(score, listUsed.length);
+        })
+    }
+}
+
+function afficherProposition(text) {
+    let zoneProposition = document.querySelector(".zoneProposition");
+    zoneProposition.innerText = text;
+}
+
+function afficherResultat(score, total) {
+    let zoneScore = document.querySelector(".zoneScore");
+
+    if (total !== undefined) {
+        zoneScore.innerText = `Votre score : ${score}/${total}`;
+    } else {
+        zoneScore.innerText = `Votre score : ${score}`;
+    }
+}
